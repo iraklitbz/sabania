7315d8c9e256b779c2ea274ab78cb06701ae5a3f
irakli <iraklitbz@gmail.com>
2024-12-12 12:01:15 +0100

    removed apple

--- components/PayPalButton.vue
+++ components/PayPalButton.vue
@@ -8,7 +8,7 @@ onMounted(async () => {
 
   if (paypal) {
     paypal.Buttons({
-      createOrder: (data, actions) => {
+      createOrder: function(data, actions) {
         return actions.order.create({
           purchase_units: [{
             amount: {
@@ -17,13 +17,13 @@ onMounted(async () => {
           }],
         });
       },
-      onApprove: (data, actions) => {
-        return actions.order.capture().then(details => {
-          console.log('Transaction completed by', details)
-          navigateTo('/success')
-        })
+      onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+          console.log('Transaction completed by', details);
+          navigateTo('/success');
+        });
       },
-      onError: (err) => {
+      onError: function(err) {
         console.error('PayPal SDK error:', err);
       },
     }).render(paypalRef.value!);
