c1c4ef41d4d72eacae1dfcd2007c3f59da174cd2
irakli <iraklitbz@gmail.com>
2024-12-17 10:54:22 +0100

    testing

--- server/api/reservations.ts
+++ server/api/reservations.ts
@@ -5,16 +5,14 @@ export default defineEventHandler(async (event): Promise<any> => {
     const body = await readBody(event);
     const [firstName, ...lastNameParts] = body.fullName.split(" ");
     const lastName = lastNameParts.join(" ");
-    const dataToSend = {
-        apartmentId: Number(body.apartmentId),
-        arrivalDate: format(new Date(body.checkin), "YYYY-MM-DD"),
-        departureDate: format(new Date(body.checkout), "YYYY-MM-DD"),
-        firstName: firstName || "",
-        lastName: lastName || "",
-        email: body.email,
-        adults: Number(body.adults),
-        price: parseFloat(body.amountPayed)
-    };
+    let dataToSend = JSON.stringify({
+        "arrivalDate": "2025-12-04",
+        "departureDate": "2025-12-05",
+        "apartmentId": 2130636,
+        "firstName": "irakli",
+        "lastName": "tbz",
+        "email": "iraklitbz@gmail.com"
+    });
 
     try {
         const response = await $fetch(`${config.public.SMOOBU_API_URL}/api/reservations`, {
@@ -23,7 +21,7 @@ export default defineEventHandler(async (event): Promise<any> => {
                 "Content-Type": "application/json",
                 "API-Key": config.public.SMOOBU_API_KEY
             },
-            body: dataToSend
+            data: dataToSend
         });
 
         return response;
